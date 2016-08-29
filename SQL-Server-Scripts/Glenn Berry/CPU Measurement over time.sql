97aa9b99bc0f3d75e23e44da0085fba716be7728
kremers78 <mark.kremers.prive@gmail.com>
2016-08-29 13:22:40 +0200

    Syntaxt of IO and CPU measurements over time

--- SQL-Server-Scripts/Glenn Berry/CPU Measurement over time.sql
+++ SQL-Server-Scripts/Glenn Berry/CPU Measurement over time.sql
@@ -1,42 +1,52 @@
+------------------------------------------------------------------------------------------
+-- Based on a script from Glenn Berry
+--
+-- Returns the amount of CPU time over a period (10 seconds)
+-- Beware, the results may be skewed, since the scripts usses the total worker time
+-- which will be higher on parallel queries (every worker is added, so every active
+-- instruction on any core will add up)
+------------------------------------------------------------------------------------------
+
 if object_id('tempdb..#first') is not null
 begin 
   drop table #first
 end
 
 
-;WITH DB_CPU_Stats
-AS
-(SELECT pa.DatabaseID, DB_Name(pa.DatabaseID) AS [Database Name], SUM(qs.total_worker_time/1000) AS [CPU_Time_Ms]
- FROM sys.dm_exec_query_stats AS qs WITH (NOLOCK)
- CROSS APPLY (SELECT CONVERT(int, value) AS [DatabaseID] 
-              FROM sys.dm_exec_plan_attributes(qs.plan_handle)
-              WHERE attribute = N'dbid') AS pa
- GROUP BY DatabaseID)
-SELECT ROW_NUMBER() OVER(ORDER BY [CPU_Time_Ms] DESC) AS [CPU Rank],
-       [Database Name], [CPU_Time_Ms] AS [CPU Time (ms)], 
-       CAST([CPU_Time_Ms] * 1.0 / SUM([CPU_Time_Ms]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [CPU Percent]
-into #first
-FROM DB_CPU_Stats
-WHERE DatabaseID <> 32767 -- ResourceDB
+;WITH DB_CPU_Stats AS (
+SELECT  pa.DatabaseID
+      , DB_Name(pa.DatabaseID) AS [Database Name]
+      , SUM(qs.total_worker_time/1000) AS [CPU_Time_Ms]
+FROM    sys.dm_exec_query_stats AS qs WITH (NOLOCK)
+        CROSS APPLY  (SELECT  CONVERT(int, value) AS [DatabaseID] 
+                      FROM    sys.dm_exec_plan_attributes(qs.plan_handle)
+                      WHERE   attribute = N'dbid') AS pa
+GROUP BY DatabaseID)
+SELECT  ROW_NUMBER() OVER(ORDER BY [CPU_Time_Ms] DESC) AS [CPU Rank]
+      , [Database Name]
+      , [CPU_Time_Ms] AS [CPU Time (ms)]
+      , CAST([CPU_Time_Ms] * 1.0 / SUM([CPU_Time_Ms]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [CPU Percent]
+into    #first
+FROM    DB_CPU_Stats
+WHERE   DatabaseID <> 32767 -- ResourceDB
 ORDER BY [CPU Rank] OPTION (RECOMPILE);
 
 waitfor delay '00:00:10'
 
-;WITH DB_CPU_Stats
-AS
-(SELECT pa.DatabaseID, DB_Name(pa.DatabaseID) AS [Database Name], SUM(qs.total_worker_time/1000) AS [CPU_Time_Ms]
- FROM sys.dm_exec_query_stats AS qs WITH (NOLOCK)
- CROSS APPLY (SELECT CONVERT(int, value) AS [DatabaseID] 
-              FROM sys.dm_exec_plan_attributes(qs.plan_handle)
-              WHERE attribute = N'dbid') AS pa
- GROUP BY DatabaseID)
---SELECT ROW_NUMBER() OVER(ORDER BY [CPU_Time_Ms] DESC) AS [CPU Rank],
---       [Database Name], [CPU_Time_Ms] AS [CPU Time (ms)], 
---       CAST([CPU_Time_Ms] * 1.0 / SUM([CPU_Time_Ms]) OVER() * 100.0 AS DECIMAL(5, 2)) AS [CPU Percent]
+;WITH DB_CPU_Stats AS (
+SELECT  pa.DatabaseID
+      , DB_Name(pa.DatabaseID) AS [Database Name]
+      , SUM(qs.total_worker_time/1000) AS [CPU_Time_Ms]
+FROM    sys.dm_exec_query_stats AS qs WITH (NOLOCK)
+        CROSS APPLY  (SELECT  CONVERT(int, value) AS [DatabaseID] 
+                      FROM    sys.dm_exec_plan_attributes(qs.plan_handle)
+                      WHERE   attribute = N'dbid') AS pa
+GROUP BY DatabaseID)
 select  c.[Database Name]
-      , c.[CPU_Time_Ms] - f.[CPU Time (ms)]
-FROM DB_CPU_Stats c
-    inner join #first f
-      on  f.[Database Name] = c.[Database Name]
-WHERE DatabaseID <> 32767 -- ResourceDB
+      , c.[CPU_Time_Ms] - f.[CPU Time (ms)] as [CPU Time (ms)]
+      , cast((c.[CPU_Time_Ms] - f.[CPU Time (ms)] ) / 1000.0 as decimal(5,2))as [CPU Time (s)]
+FROM    DB_CPU_Stats c
+        inner join #first f
+          on  f.[Database Name] = c.[Database Name]
+WHERE   DatabaseID <> 32767 -- ResourceDB
 ORDER BY 2 desc OPTION (RECOMPILE);
--- SQL-Server-Scripts/Glenn Berry/IO Measurement over time.sql
+++ SQL-Server-Scripts/Glenn Berry/IO Measurement over time.sql
@@ -1,39 +1,52 @@
+------------------------------------------------------------------------------------------
+-- Based on a script from Glenn Berry
+--
+-- Returns the amount of IO time over a period (10 seconds)
+------------------------------------------------------------------------------------------
+
+
 if object_id('tempdb..#first') is not null
 begin
   drop table #first
 end
 
-
 -- Get I/O utilization by database (Query 29) (IO Usage By Database)
-;WITH Aggregate_IO_Statistics
-AS
-(SELECT DB_NAME(database_id) AS [Database Name],
-CAST(SUM(num_of_bytes_read + num_of_bytes_written)/1048576 AS DECIMAL(12, 2)) AS io_in_mb
-FROM sys.dm_io_virtual_file_stats(NULL, NULL) AS [DM_IO_STATS]
+;WITH Aggregate_IO_Statistics AS (
+SELECT  DB_NAME(database_id) AS [Database Name]
+      , CAST(SUM(num_of_bytes_read + num_of_bytes_written)/1048576 AS DECIMAL(12, 2)) AS io_in_mb
+      , CAST(SUM(num_of_bytes_read)/1048576 AS DECIMAL(12, 2)) AS io_reads_in_mb
+      , CAST(SUM(num_of_bytes_written)/1048576 AS DECIMAL(12, 2)) AS io_writes_in_mb
+FROM    sys.dm_io_virtual_file_stats(NULL, NULL) AS [DM_IO_STATS]
 GROUP BY database_id)
-SELECT ROW_NUMBER() OVER(ORDER BY io_in_mb DESC) AS [I/O Rank], [Database Name], io_in_mb AS [Total I/O (MB)],
-       CAST(io_in_mb/ SUM(io_in_mb) OVER() * 100.0 AS DECIMAL(5,2)) AS [I/O Percent]
-into  #first
-FROM Aggregate_IO_Statistics
-ORDER BY [I/O Rank] OPTION (RECOMPILE);
+SELECT  ROW_NUMBER() OVER(ORDER BY io_in_mb DESC) AS [I/O Rank]
+      , [Database Name]
+      , io_in_mb AS [Total I/O (MB)]
+      , io_reads_in_mb AS io_reads_in_mb
+      , io_writes_in_mb AS io_writes_in_mb
+      , CAST(io_in_mb/ SUM(io_in_mb) OVER() * 100.0 AS DECIMAL(5,2)) AS [I/O Percent]
+into    #first
+FROM    Aggregate_IO_Statistics
+ORDER BY [I/O Rank] 
+OPTION (RECOMPILE);
 ------
 
 waitfor delay '00:00:10'
 
 -- Get I/O utilization by database (Query 29) (IO Usage By Database)
-;WITH Aggregate_IO_Statistics
-AS
-(SELECT DB_NAME(database_id) AS [Database Name],
-CAST(SUM(num_of_bytes_read + num_of_bytes_written)/1048576 AS DECIMAL(12, 2)) AS io_in_mb
-FROM sys.dm_io_virtual_file_stats(NULL, NULL) AS [DM_IO_STATS]
+;WITH Aggregate_IO_Statistics AS (
+SELECT  DB_NAME(database_id) AS [Database Name]
+      , CAST(SUM(num_of_bytes_read + num_of_bytes_written)/1048576 AS DECIMAL(12, 2)) AS io_in_mb
+      , CAST(SUM(num_of_bytes_read)/1048576 AS DECIMAL(12, 2)) AS io_reads_in_mb
+      , CAST(SUM(num_of_bytes_written)/1048576 AS DECIMAL(12, 2)) AS io_writes_in_mb
+FROM    sys.dm_io_virtual_file_stats(NULL, NULL) AS [DM_IO_STATS]
 GROUP BY database_id)
---SELECT ROW_NUMBER() OVER(ORDER BY io_in_mb DESC) AS [I/O Rank], [Database Name], io_in_mb AS [Total I/O (MB)],
---       CAST(io_in_mb/ SUM(io_in_mb) OVER() * 100.0 AS DECIMAL(5,2)) AS [I/O Percent]
 select  f.[database name]
-      , a.io_in_mb  - f.[Total I/O (MB)] as [Total I/O (MB)]
+      , a.io_in_mb - f.[Total I/O (MB)] as [Total I/O (MB)]
+      , a.io_reads_in_mb - f.io_reads_in_mb as [Total Reads I/O (MB)]
+      , a.io_writes_in_mb - f.io_writes_in_mb as [Total Writes I/O (MB)]
       
-FROM Aggregate_IO_Statistics A
-      inner join #first f
-        on  f.[database name] = a.[database name]
+FROM    Aggregate_IO_Statistics A
+        inner join #first f
+          on  f.[database name] = a.[database name]
 ORDER BY 2 desc OPTION (RECOMPILE);
 ------
