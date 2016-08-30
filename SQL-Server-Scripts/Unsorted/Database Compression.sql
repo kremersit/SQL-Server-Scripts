587f329677c1695955e10453cec59a89a2958996
kremers78 <mark.kremers.prive@gmail.com>
2016-08-30 11:37:31 +0200

    Table Compression
    Small changes

--- SQL-Server-Scripts/Unsorted/Database Compression.sql
+++ SQL-Server-Scripts/Unsorted/Database Compression.sql
@@ -1,23 +1,28 @@
-SELECT	
-        case 
-          when data_compression = 0 
-          then 'USE [' + db_name(db_id()) + ']; ALTER TABLE [' + s.name + '].[' + st.name + '] REBUILD PARTITION = ALL WITH (DATA_COMPRESSION = PAGE)'
+select	case 
+          when data_compression = 0 and i.type = 0 then 
+            'USE [DBA];BEGIN TRY ALTER TABLE [' + s.name + '].[' + t.name + '] REBUILD PARTITION = ALL WITH (DATA_COMPRESSION = PAGE, ONLINE = ON) END TRY BEGIN CATCH PRINT error_message() END CATCH '
+          when data_compression = 0 and i.type > 0 then 
+            'USE [DBA];BEGIN TRY ALTER INDEX [' + i.name + '] on [' + s.name + '].[' + t.name + '] REBUILD PARTITION = ALL WITH (DATA_COMPRESSION = PAGE, ONLINE = ON) END TRY BEGIN CATCH PRINT error_message() END CATCH '
           else ''
         end as sql_statement
-      , s.name
-      , st.name
-
+      , s.name as schema_name
+      , t.name as table_name
+      , i.name as index_name
       , a.total_pages * 8 / 1024.00 / 1024 AS total_gb
       , a.used_pages  * 8 / 1024.00 / 1024 AS used_gb
-	    , st.object_id
-      , sp.partition_id
-      , sp.partition_number
-      , sp.data_compression
-      , sp.data_compression_desc
-FROM    sys.partitions SP
-        INNER JOIN sys.tables ST 
-          ON  st.object_id = sp.object_id
-        inner join sys.schemas s
-          on  s.schema_id = st.schema_id
-        INNER JOIN sys.allocation_units a ON a.container_id = sp.partition_id 
-order by total_gb desc
+	    , t.object_id
+      , p.partition_id
+      , p.partition_number
+      , p.data_compression
+      , p.data_compression_desc
+from    sys.partitions p with (nolock)
+        inner join sys.tables t with (nolock)
+          on  t.object_id = p.object_id
+        inner join sys.schemas s with (nolock)
+          on  s.schema_id = t.schema_id
+        inner join sys.allocation_units a with (nolock)
+          on  a.container_id = p.partition_id 
+        left join sys.indexes i with (nolock)
+          on  i.object_id = t.object_id
+order by total_gb desc 
+option (recompile);
