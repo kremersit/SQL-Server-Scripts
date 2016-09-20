86213703e1b9be4bac8529f3841af023d6a55c03
kremers78 <mark.kremers.prive@gmail.com>
2016-09-20 12:00:24 +0200

    Minor change to ETA script

--- SQL-Server-Scripts/Unsorted/Percentage Complete inc ETA.sql
+++ SQL-Server-Scripts/Unsorted/Percentage Complete inc ETA.sql
@@ -1,5 +1,3 @@
-/*   Returns an Estimated Time of Arrival (ETA) on a longer running query  */
-
 ;with  
     dm_exec_requests as (
 select  percent_complete
@@ -11,9 +9,9 @@ where   percent_complete > 0
 ),  calculated_dm_exec_requests as (
 select  *
       , percent_complete / elapsed_time_seconds as percent_per_second
-      , (100 / (percent_complete / elapsed_time_seconds)) / 60 / 60 as total_running_time
+      , (100 / (percent_complete / elapsed_time_seconds)) as total_running_time
 from    dm_exec_requests
 )
 select  *
-      , dateadd(hour, total_running_time, dateadd(minute, total_running_time, start_time)) as ETA
+      , dateadd(SECOND, total_running_time, start_time) as ETA
 from    calculated_dm_exec_requests
