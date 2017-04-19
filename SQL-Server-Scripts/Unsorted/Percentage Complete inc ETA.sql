fe0e5ea18ad588d8b2b3347a2e50b66bf446a0a2
kremers78 <mark.kremers.prive@gmail.com>
2017-04-19 09:43:42 +0200

    Update Percentage Complete inc ETA.sql
    Added session infor

--- SQL-Server-Scripts/Unsorted/Percentage Complete inc ETA.sql
+++ SQL-Server-Scripts/Unsorted/Percentage Complete inc ETA.sql
@@ -1,11 +1,17 @@
 ;with  
     dm_exec_requests as (
-select  percent_complete
-      , start_time
+select  d.percent_complete
+      , d.start_time
       , getdate() as current_date_time
-      , datediff(s, start_time, getdate()) as elapsed_time_seconds
-from    sys.dm_exec_requests 
-where   percent_complete > 0 
+      , datediff(s, d.start_time, getdate()) as elapsed_time_seconds
+      , d.session_id
+      , case when d.statement_start_offset = 0 and d.statement_end_offset = -1 
+          then t.text
+          else substring(t.text, (d.statement_start_offset / 2), (d.statement_end_offset / 2)) 
+        end as sql_text
+from    sys.dm_exec_requests d
+        cross apply sys.dm_exec_sql_text(d.sql_handle) t
+where   d.percent_complete > 0 
 ),  calculated_dm_exec_requests as (
 select  *
       , percent_complete / elapsed_time_seconds as percent_per_second
