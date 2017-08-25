e1cd7fb420eade73345a49f3f9fadb3d3bb8d5aa
Mark Kremers <9531652+kremers78@users.noreply.github.com>
2017-08-25 08:50:32 +0200

    removed into in sp_msforeachdb statement

--- SQL-Server-Scripts/Unsorted/Free space in files.sql
+++ SQL-Server-Scripts/Unsorted/Free space in files.sql
@@ -6,18 +6,18 @@ set nocount on;
 ------------------------------------------------------------------------------------------------------------------------
 if object_id('tempdb..#database_files') is not null
 begin
-	drop table #database_files
+  drop table #database_files
 end
 go
 ------------------------------------------------------------------------------------------------------------------------
 -- Create table definition
 ------------------------------------------------------------------------------------------------------------------------
 select  top 0 
-		    db_name() AS database_name
+	db_name() AS database_name
       , name AS file_name
-	    , size / 128.0 AS file_size_in_mb
+      , size / 128.0 AS file_size_in_mb
       , size / 128.0 - cast(fileproperty(name, 'SpaceUsed') as int) / 128.0 as free_space_in_file_in_mb
-into	  #database_files
+into	#database_files
 FROM    sys.database_files; 
 ------------------------------------------------------------------------------------------------------------------------
 -- Fill temp table with result
@@ -28,12 +28,11 @@ exec sp_msforeachdb N'
 use [?];
 select  db_name() AS database_name
       , name AS file_name
-	    , size / 128.0 AS file_size_in_mb
+      , size / 128.0 AS file_size_in_mb
       , size / 128.0 - cast(fileproperty(name, ''SpaceUsed'') as int) / 128.0 as free_space_in_file_in_mb
-into	  #database_files
 FROM    sys.database_files; '
 ------------------------------------------------------------------------------------------------------------------------
 -- Output results
 ------------------------------------------------------------------------------------------------------------------------
 select	*
-from	  #database_files
+from	#database_files
