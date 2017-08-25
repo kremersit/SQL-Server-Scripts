fe710a9a6b99d04bb0829a915b0b6a8756a25918
Mark Kremers <9531652+kremers78@users.noreply.github.com>
2017-08-25 08:54:18 +0200

    More information in output (percentage and type)

--- SQL-Server-Scripts/Unsorted/Free space in files.sql
+++ SQL-Server-Scripts/Unsorted/Free space in files.sql
@@ -12,13 +12,16 @@ go
 ------------------------------------------------------------------------------------------------------------------------
 -- Create table definition
 ------------------------------------------------------------------------------------------------------------------------
-select  top 0 
-	db_name() AS database_name
+select  top 0
+        db_name() AS database_name
       , name AS file_name
       , size / 128.0 AS file_size_in_mb
       , size / 128.0 - cast(fileproperty(name, 'SpaceUsed') as int) / 128.0 as free_space_in_file_in_mb
-into	#database_files
-FROM    sys.database_files; 
+      , ((size / 128.0 - cast(fileproperty(name, 'SpaceUsed') as int) / 128.0) / (size / 128.0)) * 100.0 as percentage_free
+      , physical_name
+      , type_desc
+into	  #database_files
+from    sys.database_files; ; 
 ------------------------------------------------------------------------------------------------------------------------
 -- Fill temp table with result
 ------------------------------------------------------------------------------------------------------------------------
@@ -30,9 +33,14 @@ select  db_name() AS database_name
       , name AS file_name
       , size / 128.0 AS file_size_in_mb
       , size / 128.0 - cast(fileproperty(name, ''SpaceUsed'') as int) / 128.0 as free_space_in_file_in_mb
+      , ((size / 128.0 - cast(fileproperty(name, ''SpaceUsed'') as int) / 128.0) / (size / 128.0)) * 100.0 as percentage_free
+      , physical_name
+      , type_desc
 FROM    sys.database_files; '
 ------------------------------------------------------------------------------------------------------------------------
 -- Output results
 ------------------------------------------------------------------------------------------------------------------------
 select	*
-from	#database_files
+from  	#database_files
+where   type_desc = 'ROWS'
+order by free_space_in_file_in_mb desc
