f3ba38d1efcca9954b14f3448f87fc49dad133fb
Mark Kremers <9531652+kremers78@users.noreply.github.com>
2017-08-30 15:14:17 +0200

    Added diff column and changed layout

--- SQL-Server-Scripts/Unsorted/Statistics Information over Instance.sql
+++ SQL-Server-Scripts/Unsorted/Statistics Information over Instance.sql
@@ -13,16 +13,17 @@ select  top 0
       , o.name as table_name
      
       , p.last_updated
-      , p.rows as row_count_last_stats_update -- Total number of rows in the table or indexed view when statistics were last updated. 
-                                              --    If the statistics are filtered or correspond to a filtered index, 
-                                              --    the number of rows might be less than the number of rows in the table.
       , p.rows_sampled                        -- Total number of rows sampled for statistics calculations.
       , p.steps                               -- Number of steps in the histogram. For more information, 
                                               --    see DBCC SHOW_STATISTICS (Transact-SQL).
       , p.unfiltered_rows                     -- Total number of rows in the table before applying the filter expression (for filtered statistics).
                                               --    If statistics are not filtered, unfiltered_rows is equal to 
                                               --    the value returns in the rows column.
+      , p.rows as row_count_last_stats_update -- Total number of rows in the table or indexed view when statistics were last updated. 
+                                              --    If the statistics are filtered or correspond to a filtered index, 
+                                              --    the number of rows might be less than the number of rows in the table.
       , t.row_count as row_count_in_partition
+      , t.row_count - p.rows as row_count_diff
       , o.type_desc
       , o.create_date
       , o.modify_date
@@ -39,21 +40,24 @@ where   o.type not in ('S', 'IT')
 insert
 into    #statistics_information
 exec master..sp_MSforeachdb N'
+use [?];
+
 select  ''?'' as database_name
 
       , o.name as table_name
      
       , p.last_updated
-      , p.rows as row_count_last_stats_update -- Total number of rows in the table or indexed view when statistics were last updated. 
-                                              --    If the statistics are filtered or correspond to a filtered index, 
-                                              --    the number of rows might be less than the number of rows in the table.
       , p.rows_sampled                        -- Total number of rows sampled for statistics calculations.
       , p.steps                               -- Number of steps in the histogram. For more information, 
                                               --    see DBCC SHOW_STATISTICS (Transact-SQL).
       , p.unfiltered_rows                     -- Total number of rows in the table before applying the filter expression (for filtered statistics).
                                               --    If statistics are not filtered, unfiltered_rows is equal to 
                                               --    the value returns in the rows column.
+      , p.rows as row_count_last_stats_update -- Total number of rows in the table or indexed view when statistics were last updated. 
+                                              --    If the statistics are filtered or correspond to a filtered index, 
+                                              --    the number of rows might be less than the number of rows in the table.
       , t.row_count as row_count_in_partition
+      , t.row_count - p.rows as row_count_diff
       , o.type_desc
       , o.create_date
       , o.modify_date
@@ -69,3 +73,4 @@ where   o.type not in (''S'', ''IT'')
 
 select  *
 from    #statistics_information
+order by row_count_in_partition desc
