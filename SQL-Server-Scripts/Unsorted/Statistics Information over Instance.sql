6b23f3f977aa0b3dd2d48a6779d279a87f191c83
Mark Kremers <9531652+kremers78@users.noreply.github.com>
2017-08-30 14:40:39 +0200

    Create file

---
+++ SQL-Server-Scripts/Unsorted/Statistics Information over Instance.sql
@@ -0,0 +1,71 @@
+use master
+go
+set nocount on;
+
+if object_id('tempdb..#statistics_information') is not null
+begin
+  drop table #statistics_information
+end
+
+select  top 0
+        db_name() as database_name
+
+      , o.name as table_name
+     
+      , p.last_updated
+      , p.rows as row_count_last_stats_update -- Total number of rows in the table or indexed view when statistics were last updated. 
+                                              --    If the statistics are filtered or correspond to a filtered index, 
+                                              --    the number of rows might be less than the number of rows in the table.
+      , p.rows_sampled                        -- Total number of rows sampled for statistics calculations.
+      , p.steps                               -- Number of steps in the histogram. For more information, 
+                                              --    see DBCC SHOW_STATISTICS (Transact-SQL).
+      , p.unfiltered_rows                     -- Total number of rows in the table before applying the filter expression (for filtered statistics).
+                                              --    If statistics are not filtered, unfiltered_rows is equal to 
+                                              --    the value returns in the rows column.
+      , t.row_count as row_count_in_partition
+      , o.type_desc
+      , o.create_date
+      , o.modify_date
+into    #statistics_information
+from    sys.stats s
+        outer apply sys.dm_db_stats_properties(s.object_id, s.stats_id) p
+        left join sys.dm_db_partition_stats t
+          on  t.object_id = s.object_id
+        left join sys.objects o
+          on  o.object_id = s.object_id
+-- Filter out System tables (S) and Internal tables (IT)
+where   o.type not in ('S', 'IT')
+
+insert
+into    #statistics_information
+exec master..sp_MSforeachdb N'
+select  ''?'' as database_name
+
+      , o.name as table_name
+     
+      , p.last_updated
+      , p.rows as row_count_last_stats_update -- Total number of rows in the table or indexed view when statistics were last updated. 
+                                              --    If the statistics are filtered or correspond to a filtered index, 
+                                              --    the number of rows might be less than the number of rows in the table.
+      , p.rows_sampled                        -- Total number of rows sampled for statistics calculations.
+      , p.steps                               -- Number of steps in the histogram. For more information, 
+                                              --    see DBCC SHOW_STATISTICS (Transact-SQL).
+      , p.unfiltered_rows                     -- Total number of rows in the table before applying the filter expression (for filtered statistics).
+                                              --    If statistics are not filtered, unfiltered_rows is equal to 
+                                              --    the value returns in the rows column.
+      , t.row_count as row_count_in_partition
+      , o.type_desc
+      , o.create_date
+      , o.modify_date
+from    ?.sys.stats s
+        outer apply ?.sys.dm_db_stats_properties(s.object_id, s.stats_id) p
+        left join ?.sys.dm_db_partition_stats t
+          on  t.object_id = s.object_id
+        left join ?.sys.objects o
+          on  o.object_id = s.object_id
+-- Filter out System tables (S) and Internal tables (IT)
+where   o.type not in (''S'', ''IT'')
+'
+
+select  *
+from    #statistics_information
